b2be004d1dde7b4ac56814dacc450b6ce05b84e3
Barrett Anderson <barrettanderson1989@gmail.com>
2020-06-12 17:16:18 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -6,16 +6,6 @@ class ChattyStudent < Student
   end
 
   def raise_hand
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-    super
-  end
+    super.times10
 
 end
